369b178086a783f81d0df8efae2df949ca316fec
Evan Davis <evan.wingfield@me.com>
2017-01-06 23:48:33 -0600

    dumb

--- Budget/Expense.swift
+++ Budget/Expense.swift
@@ -9,10 +9,31 @@
 import Foundation
 import RealmSwift
 
+
+//RANDOM THOUGHTS 
+
+// MAKE DATA IMMUTABLE! DO SOME READING
+
+// FOR DISPLAY, HAVING A MONTHLY BUDGET MIGHT BE GOOD, ALSO SHOW $$/day to spend or w/e - different ways of thinking about making that budget last through the month. 
+
+//item #1 though - nail the Expense data model, build a quick and dirty way of adding them. 
+
+//item #2 - display expenses montly
+
+//item #3 - make it pretty. 
+
+//item #4 - export to CSV 
+
+//item #5 - ship it. 
+
+// making a dumb change to get my git commit
+
+
 final class Expense: Object {
     dynamic var expenseText = ""
     dynamic var category = ""
     dynamic var amount = 0.0
+    dynamic var date = Date()
     var place = Place()
     
     
